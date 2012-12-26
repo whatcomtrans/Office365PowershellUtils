e3d1c5b79d15de3d954f6dfd4bd9d27526a5c820
R. Josh Nylander <joshn@ridewta.com>
2012-12-26 11:22:38 -0800

    Adjusted Connect-Office365 to test if connected already.  This means it can be easily included in other functions or scripts and not worry about re-invoking.

--- Function_Connect-MSOL.ps1
+++ Function_Connect-MSOL.ps1
@@ -1,27 +1,38 @@
 ﻿function Connect-Office365 {
 	[CmdletBinding(SupportsShouldProcess=$false)]
 	Param(
-		[Parameter(Mandatory=$false,Position=0,ValueFromPipeline=$false,ParameterSetName="",HelpMessage="Credentials")]
-		[System.Management.Automation.PSCredential]$Credential,
-        [Parameter(Mandatory=$false,Position=0,ValueFromPipeline=$false,ParameterSetName="",HelpMessage="Path to credentials")]
-		[String]$CredentialPath
+		[Parameter(Mandatory=$false,Position=1,HelpMessage="Credentials")]
+		    [System.Management.Automation.PSCredential]$Credential,
+        [Parameter(Mandatory=$false,HelpMessage="Path to credentials")]
+		    [String]$CredentialPath,
+        [Parameter(Mandatory=$false,HelpMessage="Forces re-connection when already connected.")]
+		    [Switch]$Force
 	)
-    
-    #Prompt for credential if not provided
-    if (!$Credential -and !$CredentialPath) {
-	    $Credential = Get-Credential #$env:username
-    }
-    if ($CredentialPath) {
-        $Credential = Import-PSCredential -Path $CredentialPath
+    #First, test if already connected
+    $existingConnection = $false
+    $existingSession = Get-PSSession -Verbose:$false | Where-Object {$_.ConfigurationName -eq "Microsoft.Exchange"} 
+    if ($existingSession -ne $null) {
+        $existingConnection = $true
     }
 
-    #Connect to MSOLService with credential
-    Connect-MsolService -Credential $Credential
+    if (!($existingConnection) -or ($Force)) {
 
-    #Connect to Exchange Online session and import
-    New-Variable -Scope "Global" -Name ExchangeOnlineSession -Value(New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/PowerShell/ -Credential $Credential -Authentication Basic -AllowRedirection)
-    #$ExchangeOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/PowerShell/ -Credential $msolcred -Authentication Basic -AllowRedirection
-    Import-PSSession $ExchangeOnlineSession -AllowClobber
-    #$_mod = Get-Module -Name "tmp_*"
-    #$_mod.AsCustomObject()
+        #Prompt for credential if not provided
+        if (!$Credential -and !$CredentialPath) {
+	        $Credential = Get-Credential #$env:username
+        }
+        if ($CredentialPath) {
+            $Credential = Import-PSCredential -Path $CredentialPath
+        }
+
+        #Connect to MSOLService with credential
+        Connect-MsolService -Credential $Credential
+
+        #Connect to Exchange Online session and import
+        New-Variable -Scope "Global" -Name ExchangeOnlineSession -Value(New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/PowerShell/ -Credential $Credential -Authentication Basic -AllowRedirection)
+        #$ExchangeOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/PowerShell/ -Credential $msolcred -Authentication Basic -AllowRedirection
+        Import-PSSession $ExchangeOnlineSession -AllowClobber
+        #$_mod = Get-Module -Name "tmp_*"
+        #$_mod.AsCustomObject()
+    }
 }
