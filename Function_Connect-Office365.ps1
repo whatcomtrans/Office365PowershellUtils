ad9131e30bfbd77eb22f59a075b9aad89ffb90e0
joshn-ridewta-com <joshn@ridewta.com>
2016-08-07 22:43:40 -0700

    Updates

--- Function_Connect-Office365.ps1
+++ Function_Connect-Office365.ps1
@@ -1,4 +1,8 @@
-﻿function global:Connect-Office365 {
+﻿<#
+See https://technet.microsoft.com/en-us/library/dn568015.aspx
+Expects all modules to be imported or available to import on demand
+#>
+function global:Connect-Office365 {
 	[CmdletBinding(SupportsShouldProcess=$false, DefaultParameterSetName="Username")]
 	Param(
 		[Parameter(Mandatory=$false,ParameterSetName="Username",HelpMessage="Credentials")]
@@ -41,9 +45,18 @@
 
         #Connect to SharePoint if available
         if (Get-Module Microsoft.Online.SharePoint.PowerShell) {
-            Import-Module Microsoft.Online.SharePoint.PowerShell
             Connect-SPOService -Credential $Credential
         }
+
+        #Connect to Skype for Business Online
+        if (Get-Module SkypeOnlineConnector) {
+            New-Variable -Scope "Global" -Name sfboSession -Value(New-CsOnlineSession -Credential $credential)
+            Import-PSSession $sfboSession
+        }
+
+        #Connect to Security & Compliance Center
+        New-Variable -Scope "Global" -Name $ccSession -Value(New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection)
+        Import-PSSession $ccSession -Prefix cc
     }
 }
-Echo "Function Connect-Office365 loaded"
+#Echo "Function Connect-Office365 loaded"
