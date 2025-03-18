ad5895623e9bc351f8753d63e09027850020960c
Josh Nylander <joshn@ridewta.com>
2025-03-18 10:36:50 -0700

    Fix for ExchangeOnline MFA ISE issue

--- Office365PowerShellUtils_mod.psm1
+++ Office365PowerShellUtils_mod.psm1
@@ -838,7 +838,14 @@ function Connect-Office365 {
                     Write-Warning -Message "Exchange not loaded.  Currently no none MFA connection to Exchange Online available, skipping connection to Exchange Online"
                 }
             } else {
-                Connect-ExchangeOnline
+                # https://david-homer.blogspot.com/2025/01/exchange-online-management-powershell.html
+                $msalPath = [System.IO.Path]::GetDirectoryName((Get-Module ExchangeOnlineManagement).Path);
+                Add-Type -Path "$msalPath\Microsoft.IdentityModel.Abstractions.dll";
+                Add-Type -Path "$msalPath\Microsoft.Identity.Client.dll";
+                [Microsoft.Identity.Client.IPublicClientApplication] $application = [Microsoft.Identity.Client.PublicClientApplicationBuilder]::Create("fb78d390-0c51-40cd-8e17-fdbfab77341b").WithDefaultRedirectUri().Build();
+                $result = $application.AcquireTokenInteractive([string[]]"https://outlook.office365.com/.default").ExecuteAsync().Result;
+                Connect-ExchangeOnline -AccessToken $result.AccessToken -UserPrincipalName $result.Account.Username; 
+                # Connect-ExchangeOnline
             }
         }
 
--- Office365PowershellUtils.psd1
+++ Office365PowershellUtils.psd1
@@ -12,7 +12,7 @@
 # RootModule = ''
 
 # Version number of this module.
-ModuleVersion = '3.0.0.5'
+ModuleVersion = '3.0.0.6'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
